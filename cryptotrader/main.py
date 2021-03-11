c6933c89f451f5eb93834295c5f64584dcf22350
Ben Watson <benwatson528@gmail.com>
2021-03-11 19:51:34 +0000

    Updating README

--- cryptotrader/main.py
+++ cryptotrader/main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import pandas as pd
 
-# Data from https://www.kaggle.com/mczielinski/bitcoin-historical-data
 df = pd.read_csv('historic_bitcoin.zip')
 df['Timestamp'] = [datetime.utcfromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S') for x in df['Timestamp']]
 
